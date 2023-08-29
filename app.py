419cf1e8522c4b16d74cb48829627a286e51a10c
TosinP <oluyemitosin72@gmail.com>
2023-08-29 21:48:28 +0100

    Getting started
    The framework is set. The next plan is to work on the home page to taste before adding some more pages

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('home_page.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
