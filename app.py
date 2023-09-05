b31fc26a992b4f572207a38465d642b08a47ea6b
TosinP <oluyemitosin72@gmail.com>
2023-09-05 22:49:43 +0100

    Database set
    The connection to Airtable is set. Much to be done but we meuve. Article structure getting loaded too.

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from database import load_articles_from_airtable, load_article_from_airtable
 
 app = Flask(__name__)
+
+
+
 
 @app.route('/')
 def home_page():
     return render_template('home_page.html')
+
+@app.route('/articles')
+def article_list_page():
+    return render_template('article_list.html')
+
+
+@app.route('/api/articles')
+def article_api_page():
+    blogs = load_articles_from_airtable()
+    return jsonify(blogs)
+
+@app.route('/articles/<id>')
+def each_article_page(id):
+    """Takes in the record ID and loads the dictionary as blog"""
+    blog = load_article_from_airtable(id)
+    return render_template('article_page.html', blog=blog)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,7 +1,29 @@
-from pyairtable import Table
+from pyairtable import Table, Base, Api
 import os
 
-api_key = 
-table = Table(base_id, base_name, api_key)
+# api_key is a general variable but the base_id is specifically for the HGRMSF base
+api_key = 'patZeMBA4dNElLfbp.fb12b8d190b52d1abc53d92308dd292f3bbe760bee758ac4ba57ebbf7dfbf576'
+base_id = 'app4a29GBUnVXWIBt'
 
-print('Done')
+# This is our callpoint
+api = Api(api_key)
+
+
+
+# This is the instance of the Articles table.
+# Each article has tables [ID, Title, Text, Writers, Photo and Date]
+Articles = api.table(base_id, 'Articles')
+
+
+def load_articles_from_airtable():
+    """Returns a list of dictionaries of articles in the table"""
+    return Articles.all()
+
+
+def load_article_from_airtable(id):
+    """Pulls a specific article dictionary"""
+    if len(Articles.get(id)) == 0:
+        return None
+    else:
+        return Articles.get(id)
+
