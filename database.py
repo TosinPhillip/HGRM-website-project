001a8f3b059e6023027b32259a26ead879dd9b02
TosinPhillip <oluyemitosin72@gmail.com>
2023-09-15 03:19:52 +0100

    added the airtable secret codes to env group

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from pyairtable import Api
+import os
 
 
 # api_key is a general variable but the base_id is specifically for the HGRMSF base
-api_key = 'patZeMBA4dNElLfbp.fb12b8d190b52d1abc53d92308dd292f3bbe760bee758ac4ba57ebbf7dfbf576'
-base_id = 'app4a29GBUnVXWIBt'
+api_key = os.environ.get('AIRTABLE_API')
+base_id = os.environ.get('HGRMSF_BASE_ID')
 
 # This is our callpoint
 api = Api(api_key)
@@ -13,7 +14,6 @@ api = Api(api_key)
 # This is the instance of the Articles table.
 # Each article has tables [ID, Title, Text, Writers, Photo and Date]
 Articles = api.table(base_id, 'Articles')
-
 
 def load_articles_from_airtable():
     """Returns a list of dictionaries of articles in the table"""
@@ -25,9 +25,8 @@ def load_article_from_airtable(id):
     return Articles.get(id)
 
 
-x = Articles.get('recN8Y2TZ5sX33RZd')['fields']['Photo']
-print(x[0]['thumbnails']['full']['url'])
-
-
+# x = Articles.get('recN8Y2TZ5sX33RZd')['fields']['Photo']
+# # print(x[0]['thumbnails']['full']['url'])
+# print(x)
 
 
