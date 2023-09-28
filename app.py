e4e62019ce875867fcd1fe042961ac55e4ccf186
TosinPhillip <oluyemitosin72@gmail.com>
2023-09-28 02:15:34 +0100

    got to paginate articles at last
    but the nav buttons don't work yet

--- app.py
+++ app.py
@@ -10,27 +10,34 @@ app = Flask(__name__)
 def home_page():
     return render_template('home_page.html')
 
+@app.route('/admin')
+def admin_page():
+    return render_template('admin_profile.html')
 
-@app.route('/articles')
-def article_list_page():
-    blogs = load_articles_from_airtable()
+
+@app.route('/articles/<int:page>')
+def article_list_page(page):
+    blogs = load_articles_from_airtable(page)
     return render_template('article_list.html', blogs=blogs)
 
 
-@app.route('/api/articles/<id>')
+@app.route('/api/article/<id>')
 def article_api_page(id):
     blogs = load_article_from_airtable(id)
     return jsonify(blogs)
 
-@app.route('/articles/<id>')
+@app.route('/article/<id>')
 def each_article_page(id):
     """Takes in the record ID and loads the dictionary as blog"""
     blog = load_article_from_airtable(id)
     return render_template('article_page.html', blog=blog)
 
-@app.route('/about')
-def about_us:
-    return render_template('about_us.html')
+# @app.route('/about')
+# def about_us:
+#     return render_template('about_us.html')
+
+
+
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -3,30 +3,50 @@ import os
 
 
 # api_key is a general variable but the base_id is specifically for the HGRMSF base
-api_key = os.environ.get('AIRTABLE_API')
-base_id = os.environ.get('HGRMSF_BASE_ID')
+# api_key = os.environ.get('AIRTABLE_API')
+# base_id = os.environ.get('HGRMSF_BASE_ID')
+
+api_key = 'patZeMBA4dNElLfbp.fb12b8d190b52d1abc53d92308dd292f3bbe760bee758ac4ba57ebbf7dfbf576'
+base_id = 'app4a29GBUnVXWIBt'
 
 # This is our callpoint
-api = Api(api_key)
+api = Api(api_key) # type: ignore
 
 
 
 # This is the instance of the Articles table.
 # Each article has tables [ID, Title, Text, Writers, Photo and Date]
-Articles = api.table(base_id, 'Articles')
+Articles = api.table(base_id, 'Articles') # type: ignore
 
-def load_articles_from_airtable():
+ITEMS_PER_PAGE=10
+
+def load_articles_from_airtable(page):
     """Returns a list of dictionaries of articles in the table"""
-    return Articles.all()
+    p = []
+    for a in Articles.iterate(page_size=ITEMS_PER_PAGE):
+        p.append(a)
+    return p[page]
 
 
 def load_article_from_airtable(id):
     """Pulls a specific article dictionary"""
     return Articles.get(id)
 
+# def paginate(list_bunch=None, item_number=10):
+#     """This function gets a list of items and an item number. It then returns the list in batches of item number"""
+#     # bunch_length = len(list_bunch)
+#     # number_of_pages = None
+    
+    
+
+
 
 # x = Articles.get('recN8Y2TZ5sX33RZd')['fields']['Photo']
 # # print(x[0]['thumbnails']['full']['url'])
-# print(x)
+# 
+# 
+blog = load_articles_from_airtable(0)
+
+print(type(blog))
 
 
